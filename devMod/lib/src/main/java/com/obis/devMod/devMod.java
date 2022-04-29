4c00b074ad025994b9e2b1bc27a66d3aa3860f6b
dltc0102 <daniellautc@gmail.com>
2022-04-29 21:07:22 +0100

    initial commit

---
+++ devMod/lib/src/main/java/com/obis/devMod/devMod.java
@@ -0,0 +1,5 @@
+package com.obis.devMod;
+
+public class devMod {
+
+}
---
+++ src/main/resources/me/obis/devMod.java
@@ -0,0 +1,16 @@
+
+/*
+package me.obis;
+
+import net.minecraft.init.Blocks;
+
+@Mod
+(modid = devMod.MODID, name = devMod.NAME, version = devMod.VERSION)
+
+public class devMod {
+    public static final String MODID = "devMod";
+    public static final String NAME = "obisMod";
+    public static final String VERSION = ".01";
+}
+
+*/
